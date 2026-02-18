4315e925630d5922bf35ffcb549e5428606c8b1c
UbaydR <131396971+UbaydR@users.noreply.github.com>
2026-02-18 01:13:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,58 @@ with open('./data.json') as f:
 
 
 @app.get('/')
-def hello_world():
+async def hello_world():
     return 'Hello, World!'
+
+### New Function
+@app.get('/students')
+async def get_students(pref=None):
+    if pref:
+        filtered_students = []
+        for student in data:
+            if student['pref'] == pref: # select only the students with a given meal preference
+              filtered_students.append(student) # add match student to the result
+        return filtered_students
+    return data
+### End of new function
+
+@app.get('/students/{id}')
+async def get_student(id):
+  for student in data: 
+    if student['id'] == id: # Only return the student if the ID matches
+      return student
+
+##exercise 1
+@app.get("/stats")
+async def get_stats():
+    stats={}
+
+    for student in data:
+        pref=student.get("pref")
+        if pref:
+            stats[pref]=stats.get(pref,0)+1
+
+            programme=student.get("programme")
+            if programme:
+                stats[programme]=stats.get(programme,0)+1
+
+    return stats
+
+##exercise 2    
+@app.get("/add/{a}/{b}")
+async def add(a: float, b:float):
+    return {"result":a+b}
+
+@app.get("/subtract/{a}/{b}")
+async def subtract(a: float, b:float):
+    return {"result":a-b}
+
+@app.get("/multiply/{a}/{b}")
+async def multiply(a: float, b:float):
+    return {"result":a*b}
+
+@app.get("/divide/{a}/{b}")
+async def divide(a: float, b:float):
+    if b==0:
+        return{"ERROR": "Can't divide by 0"}
+    return {"result":a/b}
